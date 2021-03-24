da7b08ba90172cf48fcb439e98017d7fc74030dc
JohnSmith <salapin.danil1@mail.ru>
2021-03-24 19:47:54 +0000

    Added contact form

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 
 import Header from './components/Header';
 import Footer from './components/Footer';
+import ContactForm from './components/ContactForm';
 
 function App() {
   return (
     <div className='App'>
       <Header />
+      <ContactForm />
       <Footer />
     </div>
   );
